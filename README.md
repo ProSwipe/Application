247df98877eacd7343e9f85668c84f4e13312e18
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2023-09-19 09:34:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Development
+
+#1 Initialize all JS libraries
+
+```
+npm i
+```
+
+#2 Start the Tailwind.css CLI build process (do not close this process during your development session)
+
+```
+npx tailwindcss -i ./tailwind.css -o ./app/assets/css/output.css --watch
+```
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/*.php",
+    "./app/**/*.php",
+    "./app/**/**/*.php",
+    "./app/**/**/**/*.php",
+    "./app/**/**/**/**/*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
